df2f241d5962818023ab6e085975d430c7359559
muskan s b <01fe22bca156@kletech.ac.in>
2024-02-20 09:53:03 +0530

    updated code for calculator

---
+++ Extended_Cal.java
@@ -0,0 +1,31 @@
+/**
+ * The Calculator class extends the ExtendedCal class.
+ */
+class Extended_Cal{
+    /**
+     * The function "square" takes a float as input and returns the square of that number.
+     * 
+     * @param op1 The parameter "op1" is a float type variable that represents the operand for which we
+     * want to calculate the square.
+     * @return the square of the input number.
+     */
+    float square(float op1){
+        return(op1*op1);
+    }
+    /**
+     * The function cube takes a float as input and returns the cube of that number.
+     * 
+     * @param op1 The parameter "op1" is a float type variable that represents the operand for the cube
+     * function.
+     * @return The cube of the input number.
+     */
+    float cube(float op1){
+        return(op1*op1*op1);
+    }
+    // The `float mod(float op1, op2)` function in the Calculator class is calculating the modulus of
+    // two numbers.
+   
+    }
+
+
+
